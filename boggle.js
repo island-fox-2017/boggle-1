119d75bac8d49c339571e1724389b9f1b134eca2
Patrick Benz <patrickbenz.09@gmail.com>
2017-07-08 14:44:19 +0700

    Boggle 1 DONE

---
+++ boggle.js
@@ -0,0 +1,32 @@
+'use strict'
+
+class BoggleBoard {
+  constructor() {
+    this.data = [];
+    this.size = 0;
+  }
+
+  shake(size) {
+    this.size = size;
+    this.metaBoardPhosis();
+    console.log(this.data);
+  }
+
+  metaBoardPhosis() {
+    for (let i = 0; i < this.size; i++) {
+      this.data.push([]);
+      for (let j = 0; j < this.size; j++) {
+        this.data[i].push(this.generateRandomAlphabet())
+      }
+    }
+  }
+
+  generateRandomAlphabet() {
+    return String.fromCharCode(65 + Math.floor(Math.random()*26+1));
+  }
+}
+
+let args = process.argv.splice(2).join();
+
+let board = new BoggleBoard();
+board.shake(args?args:4);
