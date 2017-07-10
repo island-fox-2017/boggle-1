ebaaac53f57b5da1acfd19c62778555405a11435
Adith <adithwidyapradipta@gmail.com>
2017-07-10 09:05:42 +0700

    done

--- boggle.js
+++ boggle.js
@@ -2,10 +2,18 @@
 
 function boogleBoard(size) {
     let alphabets = ['A', 'B', 'C', 'D', 'E', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    
     var board = [];
     
     for (let rows = 0; rows < size; rows++) {
-        board[rows].push([]);
+        
+        let temp = []
+//        board.push([]);
+        for (let col = 0; col < size; col++) {
+            let random = Math.floor(Math.random() * 26)
+            temp.push(alphabets[random]);
+        }
+        board.push(temp);
     }
     
     return board;
